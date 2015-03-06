599cd8d1d533b7678790ea8b38c02287101804ae
Christian Blais <christ.blais@gmail.com>
2015-03-05 21:33:07 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Spark of War
+
+
+## Content
+
+- 60 spaceships
+- 26 planets
+  * 3x Dwarf planets (value of 3)
+  * Planet (value of 4)
+  * Planet (value of 5)
+  * Planet (value of 6)
+  * Planet (value of 7)
+  * Star (value of 8)
+- 12 supply stations
+- 6 reminder cards
+
+## Background and Context
+This game is about war, and the only way to win is by claiming stars and planets for your empire. In order to do so, you deploy your armada in the galaxy - destroy enemy ships, expand your empire, and claim the spoils of war for yourself.
+
+
+##Objectives
+The game ends when all star(s) have been claimed, at which time players will count the number of planets they have conquered - each conquered planet is worth 1 point, and some combinations may yield bonuses (see scoring section of this rule booklet)
+
+
+## Setup
+1. Pick a map (see appendix section of this booklet - advanced players may choose to create their own map)
+- Place the planets on the playing field in accordance with the selected map configuration (from now on we will refer to this setting as, the "galaxy"). Any unused planet can be removed from the game.
+- Shuffle the spaceship cards (supply stations do not count as spaceships!) and place them face down next to the galaxy. 
+- Players determine the order in which they will play. The player who most recently went in space starts... otherwise we assume you are smart enough to figure out a way to select who goes first... if not the player on your left is the first player.
+- The first and second player draw one and two spaceship cards respectively. All other players pick 3 spaceship cards.
+
+## The Mechanics
+Planets, spaceships and supply stations have a set number of resistance points attributed to them (that is, how much damage they can sustain before being conquered). The number of resistance points is clearly identified in the #$^#% symbol on each card. 
+
+Spaceships deal damage in one or more directions. The strength and direction of the damage dealt is displayed on the cards in the #%^$%&^ symbols.
+
+A planet or spaceship is claimed when it receives damage greater or equal to its resistance points. 
+
+Most planets are equipped with forcefields to protect them from invaders. Forcefields block all damage dealt in their direction, and can be identified on the border of planet cards with the following symbol %^$%^ 
+
+For those of you who do better with visual support - you may refer to the illustrative examples in the right margin of this page.
+
+
+## Turns
+A player's turn is played in 5 steps. Once a turn is completed, the player to your left begins his.
+
+1. Deployment
+2. Special powers
+3. Damage
+4. Conquer
+5. Play Again
+6. Draw and Discard
+
+### 1. Deployment
+
+During this step, the player must deploy a spaceship or supply station from his hand into the galaxy. Spaceships and supply stations may only be played next to another card. Additionally, they may not be placed in a position where they would sustain damage greater or equal to their resistance points.
+
+### 2. Special Powers
+
+Some spaceships have special powers, and they activate immediately after being deployed.
+
+- %$^#$ : You may discard a spaceship or supply station from your hand
+- $%^$%^: You may draw a spaceship
+- %^%$^$: Rotate a planet into desired position
+- $%^$% : Rotate a spaceship or supply station into desired position
+
+
+### 3. Damage
+
+All spaceships and supply stations deal their damage simultaneously. The player may choose to discard claimed spaceship) to deal additional damage - for every two salvage discarded, deal 1 additional damage to a target of your choice. Salvage discarded in this way are placed back into the spaceship discard pile. 
+
+
+### 4. Conquer
+
+Claim all planets and spaceships that sustain damage greater or equal to their resistance points. 
+
+* Claimed planets are placed next to you and set aside until the scoring phase. 
+* Claimed spaceships are placed face up next to you and can be used later on during your turn to deal additional damage (see Damage section)
+
+Supply stations that sustain damage greater or equal to their reisitance points are destroyed - they are placed back into the supply station pile.
+
+
+### 5. Play Again
+
+A player may choose to play steps 1 to 4 a second time in his turn. If he does, he draws a supply station at the end.
+
+This can be useful as it will allow the player to deploy two vessels in his turn.
+The drawback, however, is that the player will draw a supply station at the end of his turn... and supply stations are very weak.
+
+
+### 6. Draw and Discard
+
+At the end of you turn, if you have no cards remaining in your hand, draw 3 cards from the spaceship draw pile.
+Discard all claimed spaceship cards in excess of 2.
+
+
+## Special powers
+
---
+++ src/spark.js
@@ -0,0 +1,271 @@
+var Tile = React.createClass({
+  statics: {
+    parsePosition: function(string) {
+      var position = string.split(',');
+
+      var x = parseInt(position[0]);
+      var y = parseInt(position[1]);
+
+      return [x, y];
+    },
+
+    neighbourCoordinates: function (string) {
+      position = Tile.parsePosition(string);
+
+      r = position[0];
+      q = position[1];
+
+      return [
+        [r - 1, q],
+        [r - 1, q + 1],
+        [r, q + 1],
+        [r + 1, q],
+        [r + 1, q - 1],
+        [r, q - 1],
+      ];
+    },
+
+    hexToPixels: function(position) {
+      position = Tile.parsePosition(position);
+
+      r = position[0];
+      q = position[1];
+
+      x = 70 * 3 / 2 * q;
+      y = 70 * Math.sqrt(3) * (r + q / 2);
+
+      return [x, y];
+    }
+  },
+
+  getCard: function() {
+    var card = this.props.card;
+
+    if (card === null)
+      return <div className="card card--empty" />
+    else
+      return <Card {...card} />
+  },
+
+  handleClick: function(event) {
+    if (this.props.onClick !== undefined)
+      this.props.onClick(this.props.position, this.props.card)
+  },
+
+  render: function() {
+    var coord = Tile.hexToPixels(this.props.position)
+    var style = {left: coord[0] + 'px', top: coord[1] + "px"}
+    var klass = "map__tile position--" + this.props.position;
+
+    return(
+      <div className={klass} style={style} onClick={this.handleClick}>
+        {this.getCard()}
+      </div>
+    );
+  }
+});
+
+var Card = React.createClass({
+  render: function() {
+    var klass = "card card--" + this.props.type;
+
+    return(
+      <div className={klass} onClick={this.props.onClick}>
+        {this.props.damages.map(function(damage, index) {
+          if (damage) {
+            var className = "card__side--" + (index + 1);
+
+            return(
+              <div className={className} key={index}>
+                <div className="card__side__damage">{damage}</div>
+              </div>
+            );
+          }
+        })}
+
+        {this.props.walls.map(function(wall, index) {
+          if (wall) {
+            var className = "card__side--" + (index + 1);
+
+            return(
+              <div className={className} key={index}>
+                <div className="card__side__wall"></div>
+              </div>
+            );
+          }
+        })}
+      </div>
+    );
+  }
+});
+
+var Hand = React.createClass({
+  handleClick: function(card) {
+    if (this.props.onClick !== undefined)
+      this.props.onClick(card);
+  },
+
+  render: function() {
+    return(
+      <div className="hand">
+        {this.props.cards.map(function(card, index) {
+          return <Card key={index} {...card} onClick={this.handleClick.bind(this, card)} />
+        }, this)}
+      </div>
+    );
+  }
+});
+
+var Map = React.createClass({
+  render: function() {
+    return(
+      <div className="map">
+        {Object.keys(this.props.tiles).map(function(position, index) {
+          return <Tile key={index} position={position} card={this.props.tiles[position]} onClick={this.props.onClick} />
+        }, this)}
+      </div>
+    );
+  }
+});
+
+var Spark = React.createClass({
+  getDefaultProps: function() {
+    return {
+      map: {
+        "0,0": {type: 'planet', health: 1, damages: [0,0,0,0,0,0], walls: [0,0,1,0,0,1]},
+        "1,0": {type: 'planet', health: 1, damages: [0,0,0,0,0,0], walls: [0,0,1,1,0,0]},
+        "0,1": {type: 'planet', health: 1, damages: [0,0,0,0,0,0], walls: [1,0,0,0,1,0]},
+      },
+
+      deck: [
+        {type: 'spaceship', health: 1, damages: [1,0,1,0,1,0], walls: [0,0,0,0,0,0]},
+        {type: 'spaceship', health: 1, damages: [0,2,0,0,1,0], walls: [0,0,0,0,0,0]},
+        {type: 'spaceship', health: 1, damages: [0,0,0,0,0,0], walls: [0,0,0,0,0,0]},
+      ]
+    }
+  },
+
+  getInitialState: function() {
+    return {
+      tiles: this.props.map,
+      card: null
+    }
+  },
+
+  componentDidMount: function() {
+    this.tick();
+  },
+
+  fillAvailableTiles: function() {
+    Object.keys(this.getOccupiedTiles()).forEach(function(position) {
+      Tile.neighbourCoordinates(position).forEach(function(neighbourPosition) {
+        if (this.state.tiles[neighbourPosition] === undefined) {
+          this.addTile(neighbourPosition, null);
+        }
+      }, this);
+    }, this);
+  },
+
+  addTile: function(position, card) {
+    this.state.tiles[position] = card;
+  },
+
+  removeTile: function(position) {
+    delete this.state.tiles[position];
+  },
+
+  getOccupiedTiles: function() {
+    var tiles = {}
+
+    Object.keys(this.state.tiles).forEach(function(position, index) {
+      if (card = this.state.tiles[position]) {
+        tiles[position] = card;
+      }
+    }, this)
+
+    return tiles;
+  },
+
+  getCompoundedDamage: function(position, card) {
+    var compoundedDamage = 0;
+
+    Tile.neighbourCoordinates(position).forEach(function(neighbourPosition, index) {
+      if (!card.walls[index]) {
+        if (neighbourCard = this.state.tiles[neighbourPosition]) {
+          compoundedDamage += neighbourCard.damages[(index + 3) % 6];
+        }
+      }
+    }, this)
+
+    return compoundedDamage;
+  },
+
+  removeDestroyedTiles: function() {
+    Object.keys(this.getOccupiedTiles()).forEach(function(position) {
+      var card = this.state.tiles[position];
+
+      if (this.getCompoundedDamage(position, card) >= card.health)
+        this.removeTile(position);
+    }, this);
+  },
+
+  removeEmptyTiles: function() {
+    Object.keys(this.state.tiles).forEach(function(position) {
+      if (this.state.tiles[position] === null) {
+        this.removeTile(position);
+      }
+    }, this);
+  },
+
+  removeOrphanTiles: function() {
+    Object.keys(this.getOccupiedTiles()).forEach(function(position) {
+      var kill = true;
+
+      Tile.neighbourCoordinates(position).forEach(function(neighbourPosition) {
+        if (this.state.tiles[neighbourPosition])
+          kill = false;
+      }, this);
+
+      if (kill)
+        this.removeTile(position);
+    }, this);
+  },
+
+  tick: function() {
+    this.removeDestroyedTiles();
+    this.removeEmptyTiles();
+    this.removeOrphanTiles();
+    this.fillAvailableTiles();
+
+    // final render
+    this.setState({card: null, tiles: this.state.tiles})
+  },
+
+  mapClickHandler: function(position, card) {
+    if (this.state.card === null || card !== null)
+      return
+
+    if (this.getCompoundedDamage(position, this.state.card) >= this.state.card.health)
+      return
+
+    this.addTile(position, this.state.card);
+
+    this.tick();
+  },
+
+  handClickHandler: function(card) {
+    this.setState({card: card});
+  },
+
+  render: function() {
+    console.log('render')
+    return(
+      <div className="spark__container">
+        <Map ref="map" tiles={this.state.tiles} onClick={this.mapClickHandler} />
+        <Hand ref="hand" cards={this.props.deck} onClick={this.handClickHandler} />
+      </div>
+    );
+  }
+});
+
+React.render(<Spark />, document.getElementById('spark'))
